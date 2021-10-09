6e852045adbdaf6eb2b6fa0c170a9f689df25256
JohnnyWiseau141 <john.a.foley141@gmail.com>
2021-10-09 14:54:24 -0400

    use template literal for prompt message

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   previousNum: [],
+  getGuesses: function() {
+    prompt(`Enter a guess between ${game.smallestNum} and ${game.biggestNum}:`)
+  },
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      game.getGuesses()
   },
 
   //pop up that prompts user to enter the number
-  getGuesses = prompt(`Enter a guess between [smallestNum] and [biggestNum]:`)
+  
 }
-
