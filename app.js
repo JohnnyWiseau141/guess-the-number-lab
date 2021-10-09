59c9cb586cf5c320ca7bab8cc885e8a261b8ebdd
JohnnyWiseau141 <john.a.foley141@gmail.com>
2021-10-09 10:29:32 -0400

    add prompt

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  previousNum: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+  },
+
+  //pop up that prompts user to enter the number
+  getGuesses = prompt(`Enter a guess between [smallestNum] and [biggestNum]:`)
 }
+
